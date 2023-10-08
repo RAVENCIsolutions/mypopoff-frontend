4541362e948ccd8b3c705ec9381f92508eae931e
Richard H <richard@haddads.net.au>
2023-10-09 03:34:21 +1000

    Update layout.js

--- src/app/(public)/layout.js
+++ src/app/(public)/layout.js
@@ -1,25 +1,19 @@
-"use client";
-
 import "./globals.scss";
-import "@radix-ui/themes/styles.css";
-import { darkModeContext } from "@/stores/DarkModeStore";
-import { observer } from "mobx-react";
 
-const RootLayout = observer(({ children }) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="stylesheet" href="https://use.typekit.net/oya4ufz.css" />
       </head>
       <body
-        className={
-          darkModeContext.darkMode + " bg-primary-light dark:bg-primary-dark"
+        className={"bg-primary-light dark:bg-primary-dark"
         }
       >
         {children}
       </body>
     </html>
   );
-});
+};
 
 export default RootLayout;
