33e6c804a8b998ceae193ebb956489e2ce2a97fa
richardhaddadau <richard@haddads.net.au>
2023-10-10 09:06:47 +1000

    mobile menu optimised

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,45 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { CgCloseR, CgMenuBoxed } from "react-icons/cg";
 
 const NavBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    menuOpen
+      ? document.body.classList.add("overflow-hidden")
+      : document.body.classList.remove("overflow-hidden");
+  }, [menuOpen]);
 
   return (
     <nav className="relative my-10 px-5 md:px-14 flex items-center w-full max-w-windowed">
       {/*<DarkModeToggle />*/}
       {menuOpen ? (
         <div
-          className="block md:hidden absolute -top-10 left-0 w-screen h-screen bg-primary-dark/50 z-30"
+          className="block md:hidden absolute -top-10 left-0 w-screen h-screen bg-primary-light/50 dark:bg-primary-dark/50 z-20"
           onClick={() => setMenuOpen(false)}
         />
       ) : null}
       <div
         className={
-          "absolute md:relative px-6 md:p-0 top-10 md:top-0 right-0 flex flex-col md:flex-row items-start" +
-          " md:items-center justify-start md:justify-between gap-6 md:gap-0 w-full text-primary-dark" +
-          " dark:text-primary-light bg-secondary-light md:bg-transparent h-fit md:h-auto w-max md:w-full" +
-          " overflow-hidden transition-all duration-300 z-40 " +
-          (menuOpen ? "translate-x-0" : "translate-x-full md:translate-x-0")
+          "absolute md:relative pl-6 pr-20 md:p-0 -top-10 md:top-0 right-0 flex flex-col md:flex-row items-start" +
+          " md:items-center justify-start md:justify-between gap-6 md:gap-0 bg-secondary-dark" +
+          " dark:bg-secondary-light md:bg-transparent h-screen md:h-auto w-max md:w-full text-primary-dark dark:text-primary-light text-xl md:text-base transition-all duration-300 z-50 " +
+          (menuOpen
+            ? "translate-x-0"
+            : "translate-x-full" + " md:translate-x-0")
         }
       >
-        <ul className="mt-10 md:mt-0">
+        <div className="cursor-pointer absolute -left-12 top-7 text-primary-dark dark:text-primary-light z-50">
+          {menuOpen ? (
+            <CgCloseR size={26} onClick={() => setMenuOpen(false)} />
+          ) : (
+            <CgMenuBoxed size={30} onClick={() => setMenuOpen(true)} />
+          )}
+        </div>
+
+        <ul className="mt-20 md:mt-0">
           <li>Explore</li>
         </ul>
 
@@ -37,11 +53,8 @@ const NavBar = () => {
         </ul>
       </div>
 
-      <div className="md:absolute flex flex-row items-center top-1/2 left-5 md:left-1/2 -translate-y-1/2 md:-translate-x-1/2 z-50">
-        <p
-          className="text-lg text-primary-dark dark:text-primary-light font-bold font-display"
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
+      <div className="md:absolute flex flex-row items-center justify-between top-1/2 left-5 md:left-1/2 -translate-y-1/2 md:-translate-x-1/2 w-full md:w-auto z-40">
+        <p className="text-lg text-primary-dark dark:text-primary-light font-bold font-display">
           My Pop Off
         </p>
       </div>
