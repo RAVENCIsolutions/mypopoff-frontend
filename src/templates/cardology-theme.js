2a281ccb2b1769db17023580f8e9c61acb8e02ef
richardhaddadau <richard@haddads.net.au>
2023-10-27 23:14:46 +1000

    cardology fixed for mobile

--- src/templates/cardology-theme.js
+++ src/templates/cardology-theme.js
@@ -19,16 +19,19 @@ const CardologyTheme = () => {
     {
       title: "About Me",
       content: (
-        <div className="flex flex-col items-center justify-center h-full">
+        <div className="flex flex-col items-center justify-center w-full h-full">
           <img
-            className="mb-3 w-32 h-32 rounded-full shadow-lg shadow-black/40 bg-dashboard-primary-dark object-cover object-center"
+            className="mb-8 w-32 h-32 rounded-full shadow-lg shadow-black/40 bg-dashboard-primary-dark object-cover object-center"
             src="https://img.freepik.com/free-photo/portrait-woman-with-blue-eyes_188544-7646.jpg"
             alt="Influencer Username"
           />
-          <h1 className="mb-2 font-bold text-2xl text-center tracking-wide text-sky-600">
+          <h1 className="mb-4 font-bold text-3xl text-center uppercase tracking-wide text-stone-800">
             username
           </h1>
-          <p className="text-lg text-stone-600">something about me</p>
+          <p className="text-lg font-dmsans text-stone-600 text-center max-w-xs">
+            This is a small block of text about the user who owns this
+            particular profile.
+          </p>
         </div>
       ),
     },
@@ -36,11 +39,11 @@ const CardologyTheme = () => {
     {
       title: "Links",
       content: (
-        <ul className="px-8 flex flex-col gap-5 items-stretch justify-center h-full text-center font-sans tracking-wide">
+        <ul className="px-0 md:px-8 flex flex-col gap-5 items-stretch justify-center h-full text-center">
           {sampleLinks.map((link, index) => (
             <Link
               key={index}
-              className="cursor-pointer py-2 px-5 bg-white hover:bg-sky-600 border-[3px] border-sky-600 rounded-lg text-sky-600 hover:text-white transition-all duration-300"
+              className="cursor-pointer py-2 px-0 md:px-5 bg-white hover:bg-sky-600 border-[2px] border-sky-600 rounded-lg text-sky-600 hover:text-white font-dmsans font-semibold tracking-tight transition-all duration-300"
               href={link.url}
             >
               <li>{link.title}</li>
@@ -65,16 +68,25 @@ const CardologyTheme = () => {
   const [currentCard, setCurrentCard] = useState(0);
 
   return (
-    <main className="relative px-2 sm:px-10 py-10 flex flex-col items-center justify-center w-screen h-screen min-h-fit bg-sky-600/20">
-      <section className="flex flex-row h-96 fit rounded-2xl">
+    <main className="relative px-0 md:px-10 py-20 md:py-10 flex flex-col items-center justify-center w-screen h-screen bg-sky-100 font-anton">
+      <section className="flex flex-col md:flex-row items-center justify-center w-full h-full md:h-96 rounded-2xl">
         {cards.map((card, index) => (
           <article
-            className={`relative h-full bg-white transition-all duration-1000 shadow-xl shadow-black/10 overflow-hidden ${
-              currentCard === index ? "w-[36rem]" : "w-8"
+            className={`relative h-full bg-white transition-all duration-1000 shadow-xl shadow-black/10 overflow-hidden h-full ${
+              currentCard === index
+                ? "w-full md:w-[36rem]"
+                : "w-full md:w-8 h-8 md:h-full"
             }`}
           >
             <div
-              className="cursor-pointer inline-flex items-center justify-center w-9 h-full bg-stone-700 hover:bg-stone-600 rounded-l-2xl shadow-xl shadow-black/80 border-2 border-stone-500 [writing-mode:vertical-lr] rotate-180 uppercase tracking-wider text-white transition-all duration-300"
+              className={
+                "cursor-pointer flex md:inline-flex items-center justify-center w-full md:w-9 h-9 md:h-full" +
+                " hover:bg-sky-800 shadow-md shadow-black/30 border-2 border-white md:[writing-mode:vertical-lr]" +
+                " rotate-0 md:rotate-180 uppercase text-center tracking-widest transition-all duration-300 " +
+                (currentCard === index
+                  ? "text-white bg-sky-700"
+                  : "text-white/50 bg-sky-900")
+              }
               onClick={() => {
                 if (currentCard === index) {
                   setCurrentCard(index === 0 ? 0 : index - 1);
@@ -84,7 +96,7 @@ const CardologyTheme = () => {
               {card.title}
             </div>
             <div
-              className={`absolute p-6 inline-block w-[calc(36rem-2.25rem)] h-full overflow-x-auto`}
+              className={`absolute p-6 inline-block w-full md:w-[calc(36rem-2.25rem)] h-full overflow-x-auto`}
             >
               {card.content}
             </div>
@@ -92,7 +104,7 @@ const CardologyTheme = () => {
         ))}
       </section>
 
-      <footer className="absolute bottom-3 text-sm text-stone-500">
+      <footer className="absolute bottom-3 text-sm text-sky-900/30">
         Copyright © {new Date().getFullYear()}. My Pop Off
       </footer>
     </main>
