ebf6cb813d9908d1c120324389727e5e9fe1301c
richardhaddadau <richard@haddads.net.au>
2023-11-04 22:42:16 +1000

    set up onboarding base

--- src/app/(public)/layout.js
+++ src/app/(public)/layout.js
@@ -8,16 +8,16 @@ const RootLayout = ({ children }) => {
         <link rel="stylesheet" href="https://use.typekit.net/oya4ufz.css" />
       </head>
 
-      <ClerkProvider
-        publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-        signInUrl="/login"
-        afterSignInUrl="/me/dashboard"
-        afterSignUpUrl="/me/dashboard"
-      >
-        <body className={"bg-primary-light dark:bg-primary-dark"}>
-          {children}
-        </body>
-      </ClerkProvider>
+      {/*<ClerkProvider*/}
+      {/*  publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}*/}
+      {/*  signInUrl="/login"*/}
+      {/*  afterSignInUrl="/me/dashboard"*/}
+      {/*  afterSignUpUrl="/me/dashboard"*/}
+      {/*>*/}
+      <body className={"bg-primary-light dark:bg-primary-dark"}>
+        {children}
+      </body>
+      {/*</ClerkProvider>*/}
     </html>
   );
 };
---
+++ src/app/onboarding/page.js
@@ -0,0 +1,5 @@
+const OnBoardingOne = () => {
+  return <div></div>;
+};
+
+export default OnBoardingOne;
