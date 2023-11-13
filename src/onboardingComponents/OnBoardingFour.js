45356c944f5a801682729fe108e21b150f542872
richardhaddadau <richard@haddads.net.au>
2023-11-13 18:28:49 +1000

    flesh out onboarding page 4 a little more

--- src/onboardingComponents/OnBoardingFour.js
+++ src/onboardingComponents/OnBoardingFour.js
@@ -1,12 +1,29 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { CircularProgress, TextField } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
+import { Chip, CircularProgress, TextField } from "@mui/material";
 import { TwitterPicker } from "react-color";
 import ColourPickerBlock from "@/components/ColourPickerBlock";
 import { observer } from "mobx-react";
 import onBoardingStore from "@/stores/OnBoardingStore";
 import { onBoardingButtons } from "@/data/OnBoardingButtons";
+import Uploady from "@rpldy/uploady";
+import UploadButton from "@rpldy/upload-button";
+import { TiPencil } from "react-icons/ti";
+import {
+  PiAirplaneTilt,
+  PiBarbell,
+  PiBriefcase,
+  PiDevices,
+  PiGraduationCap,
+  PiMaskHappy,
+  PiNewspaperClipping,
+  PiPaintBrushBroad,
+  PiPaintBrushHousehold,
+  PiPencilLineLight,
+  PiSword,
+} from "react-icons/pi";
+import { IoFitnessOutline } from "react-icons/io5";
 
 const OnBoardingFour = observer(() => {
   const [loading, setLoading] = useState(false);
@@ -17,6 +34,8 @@ const OnBoardingFour = observer(() => {
     tags: [],
     bio: "",
   });
+
+  const avatarOverlay = useRef(null);
 
   const handleChange = (event) => {
     setFormData({
@@ -34,9 +53,35 @@ const OnBoardingFour = observer(() => {
   return (
     <>
       <section className="relative mt-5 mb-10 self-start flex flex-col md:flex-row items-stretch justify-between">
-        <article className="pt-5 px-5 pb-10 flex flex-col items-center rounded-none md:rounded-3xl w-full bg-white shadow-lg shadow-dashboard-primary-dark/10">
+        <article className="pt-5 px-5 pb-10 flex flex-col items-center gap-8 rounded-none md:rounded-3xl w-full bg-white shadow-lg shadow-dashboard-primary-dark/10">
+          <div className="relative flex flex-col items-center gap-2 h-32 w-32 rounded-full shadow-xl shadow-primary-dark/10 overflow-hidden">
+            <img src="/images/avatar-placeholder.jpg" className="" />
+
+            <div
+              ref={avatarOverlay}
+              className="absolute flex items-center justify-center w-full h-full bg-primary-dark rounded-full opacity-0 transition-all duration-500"
+            >
+              <p className="text-white text-base">Change Avatar</p>
+            </div>
+
+            <button
+              className="avatar absolute top-2.5 right-2.5 p-2 hover:p-2.5 bg-white rounded-full shadow-md hover:shadow-xl shadow-primary-dark/10 hover:shadow-primary-dark/20 transition-all duration-300 z-20"
+              onMouseOver={() => {
+                avatarOverlay.current.style.opacity = "0.2";
+              }}
+              onMouseLeave={() => {
+                avatarOverlay.current.style.opacity = "0";
+              }}
+            >
+              <PiPencilLineLight
+                className="text-primary-dark avatar-hover:text-primary-light"
+                size={20}
+              />
+            </button>
+          </div>
+
           <div className="flex items-center gap-1">
-            <h4 className="mt-2 font-bold">mypopoff.com/</h4>
+            <h4 className="mt-2 text-2xl font-bold">mypopoff.com/</h4>
             <TextField
               label="Choose your username"
               name="username"
@@ -46,7 +91,73 @@ const OnBoardingFour = observer(() => {
             />
           </div>
 
-          <div className="grid grid-cols-1 xs:grid-cols-2 gap-6"></div>
+          <article className="flex flex-col items-center gap-3 w-full max-w-md">
+            <h4 className="mt-2 text-base font-bold">
+              Which category best suits your PopOff?
+            </h4>
+            <div className="flex flex-row justify-center flex-wrap gap-2">
+              <Chip
+                icon={<PiBriefcase />}
+                label="Business and Corporate"
+                variant="outlined"
+              />
+
+              <Chip
+                icon={<PiPaintBrushHousehold />}
+                label="Creative"
+                variant="outlined"
+              />
+
+              <Chip
+                icon={<PiGraduationCap />}
+                label="Education"
+                variant="outlined"
+              />
+
+              <Chip
+                icon={<PiMaskHappy />}
+                label="Entertainment"
+                variant="outlined"
+              />
+
+              <Chip
+                icon={<PiNewspaperClipping />}
+                label="News and Media"
+                variant="outlined"
+              />
+
+              <Chip
+                icon={<PiAirplaneTilt />}
+                label="Travel"
+                variant="outlined"
+              />
+
+              <Chip
+                icon={<PiBarbell />}
+                label="Health and Fitness"
+                variant="outlined"
+              />
+
+              <Chip
+                icon={<PiDevices />}
+                label="Technology"
+                variant="outlined"
+              />
+
+              <Chip
+                className="pl-1 bg-action text-white border-0"
+                icon={<PiSword color="white" />}
+                label="Gaming"
+                variant="outlined"
+              />
+
+              <Chip
+                icon={<PiDevices />}
+                label="Technology"
+                variant="outlined"
+              />
+            </div>
+          </article>
         </article>
       </section>
     </>
