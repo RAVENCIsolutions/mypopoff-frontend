e928c99ff61f1454dc36ddf74a69805233a1abf4
richardhaddadau <richard@haddads.net.au>
2024-02-02 08:19:10 +1000

    update tailwind for dvh height

--- src/app/(public)/colour-wheel-test/page.js
+++ src/app/(public)/colour-wheel-test/page.js
@@ -3,7 +3,7 @@
 export default function ColourWheelTestPage() {
   return (
     <main
-      className={`flex justify-center items-center w-full h-screen bg-dashboard-primary-light`}
+      className={`flex justify-center items-center w-full h-dvh bg-dashboard-primary-light`}
     >
       <ColorWheelPicker />
     </main>
