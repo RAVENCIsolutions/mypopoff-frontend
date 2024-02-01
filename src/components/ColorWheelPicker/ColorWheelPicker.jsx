08628616044582e201cff725f5d26982c1aed068
richardhaddadau <richard@haddads.net.au>
2024-02-01 23:54:57 +1000

    colour feedback block

--- src/components/ColorWheelPicker/ColorWheelPicker.jsx
+++ src/components/ColorWheelPicker/ColorWheelPicker.jsx
@@ -3,9 +3,16 @@
 import { useState } from "react";
 
 import WheelSegments from "./WheelSegments";
+import { FaSlackHash } from "react-icons/fa";
 
 const ColorWheelPicker = () => {
-  const [color, setColor] = useState("#000000");
+  const [color, setColor] = useState("000000");
+
+  const handleColorChange = (color) => {
+    console.log(color[0]);
+    const newValue = color[0] === "#" ? color.split("#")[1] : color;
+    setColor(newValue);
+  };
 
   return (
     <section className={`relative w-96`}>
@@ -24,18 +31,29 @@ const ColorWheelPicker = () => {
           "#463191",
           "#7c3697",
         ]}
-        setColor={setColor}
+        handleColorChange={handleColorChange}
       />
-      <div
+      <article
         className={`absolute flex justify-center items-center top-1/2 left-1/2 w-3/4 aspect-square bg-white rounded-full -translate-x-1/2 -translate-y-1/2 z-10`}
       >
-        <input
-          type="text"
-          value={color}
-          className={`p-1.5 w-[60%] bg-white tracking-wider text-center shadow-xl shadow-black/10 rounded-lg border-none outline-none font-semibold text-lg text-black/70`}
-          onChange={(e) => setColor(e.target.value)}
-        />
-      </div>
+        <div className={`relative w-[60%]`}>
+          <input
+            type="text"
+            value={color.toUpperCase()}
+            className={`p-1.5 pl-6 w-full bg-white tracking-widest text-center shadow-xl shadow-black/10 rounded-lg border-none outline-none font-semibold text-xl text-black/70 transition-all duration-700`}
+            style={{ borderColor: "#" + color.toUpperCase() }}
+            onChange={(e) => {
+              handleColorChange(e.target.value);
+            }}
+          />
+          <div
+            className={`absolute top-0 left-0 flex justify-center items-center h-full rounded-l-lg aspect-square`}
+            style={{ backgroundColor: "#" + color.toUpperCase() }}
+          >
+            <FaSlackHash />
+          </div>
+        </div>
+      </article>
     </section>
   );
 };
--- src/components/ColorWheelPicker/WheelSegments.jsx
+++ src/components/ColorWheelPicker/WheelSegments.jsx
@@ -20,7 +20,7 @@ const WheelSegments = ({ ...props }) => {
           strokeWidth: 0,
           transition: "all 0.3s ease",
         }}
-        onClick={() => props.setColor(props.segments[0])}
+        onClick={() => props.handleColorChange(props.segments[0])}
         d="M200,200L299.99,26.82C268.47,8.63,234.07-.01,200.12,0l-.12,200Z"
       />
       <path
@@ -30,7 +30,7 @@ const WheelSegments = ({ ...props }) => {
           strokeWidth: 0,
           transition: "all 0.3s ease",
         }}
-        onClick={() => props.setColor(props.segments[1])}
+        onClick={() => props.handleColorChange(props.segments[1])}
         d="M200,200l173.18-99.99c-18.19-31.51-43.67-56.19-73.07-73.16l-100.11,173.14Z"
       />
       <path
@@ -40,7 +40,7 @@ const WheelSegments = ({ ...props }) => {
           strokeWidth: 0,
           transition: "all 0.3s ease",
         }}
-        onClick={() => props.setColor(props.segments[2])}
+        onClick={() => props.handleColorChange(props.segments[2])}
         d="M200,200h199.97c0-36.39-9.72-70.5-26.7-99.89l-173.27,99.89Z"
       />
       <path
@@ -50,7 +50,7 @@ const WheelSegments = ({ ...props }) => {
           strokeWidth: 0,
           transition: "all 0.3s ease",
         }}
-        onClick={() => props.setColor(props.segments[3])}
+        onClick={() => props.handleColorChange(props.segments[3])}
         d="M200,200l173.18,99.99c18.19-31.51,26.83-65.92,26.82-99.86l-200-.12Z"
       />
       <path
@@ -60,7 +60,7 @@ const WheelSegments = ({ ...props }) => {
           strokeWidth: 0,
           transition: "all 0.3s ease",
         }}
-        onClick={() => props.setColor(props.segments[4])}
+        onClick={() => props.handleColorChange(props.segments[4])}
         d="M200,200l99.99,173.18c31.51-18.19,56.19-43.67,73.16-73.07l-173.14-100.11Z"
       />
       <path
@@ -70,7 +70,7 @@ const WheelSegments = ({ ...props }) => {
           strokeWidth: 0,
           transition: "all 0.3s ease",
         }}
-        onClick={() => props.setColor(props.segments[5])}
+        onClick={() => props.handleColorChange(props.segments[5])}
         d="M200,200v199.97c36.39,0,70.5-9.72,99.89-26.7l-99.89-173.27Z"
       />
       <path
@@ -80,7 +80,7 @@ const WheelSegments = ({ ...props }) => {
           strokeWidth: 0,
           transition: "all 0.3s ease",
         }}
-        onClick={() => props.setColor(props.segments[6])}
+        onClick={() => props.handleColorChange(props.segments[6])}
         d="M200,200l-99.99,173.18c31.51,18.19,65.92,26.83,99.86,26.82l.12-200Z"
       />
       <path
@@ -90,7 +90,7 @@ const WheelSegments = ({ ...props }) => {
           strokeWidth: 0,
           transition: "all 0.3s ease",
         }}
-        onClick={() => props.setColor(props.segments[7])}
+        onClick={() => props.handleColorChange(props.segments[7])}
         d="M200,200L26.82,299.99c18.19,31.51,43.67,56.19,73.07,73.16l100.11-173.14Z"
       />
       <path
@@ -100,7 +100,7 @@ const WheelSegments = ({ ...props }) => {
           strokeWidth: 0,
           transition: "all 0.3s ease",
         }}
-        onClick={() => props.setColor(props.segments[8])}
+        onClick={() => props.handleColorChange(props.segments[8])}
         d="M200,200H.03c0,36.39,9.72,70.5,26.7,99.89l173.27-99.89Z"
       />
       <path
@@ -110,7 +110,7 @@ const WheelSegments = ({ ...props }) => {
           strokeWidth: 0,
           transition: "all 0.3s ease",
         }}
-        onClick={() => props.setColor(props.segments[9])}
+        onClick={() => props.handleColorChange(props.segments[9])}
         d="M200,200L26.82,100.01C8.63,131.53-.01,165.93,0,199.88l200,.12Z"
       />
       <path
@@ -120,7 +120,7 @@ const WheelSegments = ({ ...props }) => {
           strokeWidth: 0,
           transition: "all 0.3s ease",
         }}
-        onClick={() => props.setColor(props.segments[10])}
+        onClick={() => props.handleColorChange(props.segments[10])}
         d="M200,200L100.01,26.82c-31.51,18.19-56.19,43.67-73.16,73.07l173.14,100.11Z"
       />
       <path
@@ -130,7 +130,7 @@ const WheelSegments = ({ ...props }) => {
           strokeWidth: 0,
           transition: "all 0.3s ease",
         }}
-        onClick={() => props.setColor(props.segments[11])}
+        onClick={() => props.handleColorChange(props.segments[11])}
         d="M200,200V.03C163.61.03,129.5,9.75,100.11,26.73l99.89,173.27Z"
       />
     </svg>
