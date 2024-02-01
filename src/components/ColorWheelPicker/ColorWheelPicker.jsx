321fc3d2b66103849881ef7e20412e1637e32d75
richardhaddadau <richard@haddads.net.au>
2024-02-02 00:17:51 +1000

    hash colour + pop animation

--- src/components/ColorWheelPicker/ColorWheelPicker.jsx
+++ src/components/ColorWheelPicker/ColorWheelPicker.jsx
@@ -1,36 +1,82 @@
 ﻿"use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import WheelSegments from "./WheelSegments";
 import { FaSlackHash } from "react-icons/fa";
+import { hexToRgb, rgbToHsl } from "@/utility/colourConversions";
 
 const ColorWheelPicker = () => {
   const [color, setColor] = useState("000000");
 
   const handleColorChange = (color) => {
-    console.log(color[0]);
+    document.querySelector(".color-block").style.transform = `scale(1.5)`;
+
     const newValue = color[0] === "#" ? color.split("#")[1] : color;
+    const RGBColor = hexToRgb(color);
+    const HSLColor = rgbToHsl(RGBColor);
+
+    setHashColour(HSLColor.l >= 50 ? "#141414" : "#ffffff");
+
     setColor(newValue);
+
+    setTimeout(() => {
+      document.querySelector(".color-block").style.transform = `scale(1.0)`;
+    }, 200);
   };
+
+  const [segmentColours, setSegmentColours] = useState([]);
+  const [hashColour, setHashColour] = useState("");
+
+  useEffect(() => {
+    setSegmentColours([
+      "#a71e48",
+      "#ef3224",
+      "#f15523",
+      "#f99b1e",
+      "#f9bd19",
+      "#f3ec3a",
+      "#d0dd37",
+      "#62b146",
+      "#1496ce",
+      "#3d5eac",
+      "#463191",
+      "#7c3697",
+    ]);
+
+    let startingColour = [];
+    const hexChars = [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+    ];
+
+    for (let char = 0; char < 6; char++) {
+      const newChar = hexChars[Math.floor(Math.random() * hexChars.length)];
+      console.log(newChar);
+      startingColour.push(newChar);
+    }
+
+    handleColorChange(startingColour.join(""));
+  }, []);
 
   return (
     <section className={`relative w-96`}>
       <WheelSegments
-        segments={[
-          "#a71e48",
-          "#ef3224",
-          "#f15523",
-          "#f99b1e",
-          "#f9bd19",
-          "#f3ec3a",
-          "#d0dd37",
-          "#62b146",
-          "#1496ce",
-          "#3d5eac",
-          "#463191",
-          "#7c3697",
-        ]}
+        segments={segmentColours}
         handleColorChange={handleColorChange}
       />
       <article
@@ -47,10 +93,13 @@ const ColorWheelPicker = () => {
             }}
           />
           <div
-            className={`absolute top-0 left-0 flex justify-center items-center h-full rounded-l-lg aspect-square`}
-            style={{ backgroundColor: "#" + color.toUpperCase() }}
+            className={`color-block absolute top-0 left-0 flex justify-center items-center h-full rounded-l-lg aspect-square transition-all duration-700`}
+            style={{
+              backgroundColor: "#" + color.toUpperCase(),
+              color: hashColour,
+            }}
           >
-            <FaSlackHash />
+            <FaSlackHash size={20} />
           </div>
         </div>
       </article>
---
+++ src/utility/colourConversions.js
@@ -0,0 +1,120 @@
+﻿"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.rgbToHex =
+  exports.hslToRgb =
+  exports.rgbToHsl =
+  exports.hexToRgb =
+    void 0;
+// PRIVATE
+const toHex = (value) => {
+  return value.toString(16).padStart(2, "0").toUpperCase();
+};
+const hueToRgb = (C, H) => {
+  let adjustedH = H;
+  // adjust H to be between 0 and 360
+  if (H < 0) adjustedH += 360;
+  if (H > 360) adjustedH -= 360;
+  const moddedH = (adjustedH / 60) % 2;
+  let X = 0;
+  // calculate X based on the value of (H/60)mod2
+  if (moddedH < 1) X = C * moddedH;
+  if (moddedH >= 1) X = C * (2 - moddedH);
+  // use a single "lookup" table
+  if (adjustedH < 60) return X;
+  if (adjustedH < 180) return C;
+  if (adjustedH < 240) return X;
+  return 0;
+};
+// FORWARD - HEX to HSL
+// FORWARD - Step 1. Convert Hex to RGB
+const hexToRgb = (hex) => {
+  if (hex.startsWith("#")) hex = hex.slice(1);
+  if (hex.length === 3)
+    hex = hex
+      .split("")
+      .map((char) => char + char)
+      .join(",");
+  const [rgbR, rgbG, rgbB] = [0, 2, 4]
+    .map((i) => hex.slice(i, i + 2))
+    .map((hexChar) => parseInt(hexChar, 16));
+  return { r: rgbR, g: rgbG, b: rgbB };
+};
+exports.hexToRgb = hexToRgb;
+// Step 2. Convert RGB to HSL
+const rgbToHsl = (rgb) => {
+  const rDash = rgb.r / 255;
+  const gDash = rgb.g / 255;
+  const bDash = rgb.b / 255;
+  // work out variables required to calculate HSL
+  const cMax = Math.max(rDash, gDash, bDash);
+  const cMin = Math.min(rDash, gDash, bDash);
+  const diff = cMax - cMin;
+  // calculate L
+  const resultL = (cMax + cMin) / 2;
+  // initialise H and S
+  let resultH = 0;
+  let resultS = 0;
+  // calculate H and S based on value of diff
+  if (diff === 0) {
+    resultH = 0;
+    resultS = 0;
+  } else {
+    switch (cMax) {
+      case rDash:
+        resultH = 60 * (((gDash - bDash) / diff) % 6);
+        break;
+      case gDash:
+        resultH = 60 * ((bDash - rDash) / diff + 2);
+        break;
+      case bDash:
+        resultH = 60 * ((rDash - gDash) / diff + 4);
+        break;
+    }
+    resultS = diff / (1 - Math.abs(2 * resultL - 1));
+  }
+  // return HSL values
+  // H in degrees between 0 and 360
+  // S is a value between 0 and 100
+  // L is a value between 0 and 100
+  return {
+    h: Math.round(resultH),
+    s: Math.round(resultS * 100),
+    l: Math.round(resultL * 100),
+  };
+};
+exports.rgbToHsl = rgbToHsl;
+// BACKWARD - HSL to HEX
+// BACKWARD - Step 1. Convert HSL to RGB
+const hslToRgb = (hsl) => {
+  // assume hsl.h is between 0 and 360
+  // assume hsl.s is between 0 and 100
+  // assume hsl.l is between 0 and 100
+  // convert s and l to percentages
+  const adjustedS = hsl.s / 100;
+  const adjustedL = hsl.l / 100;
+  // if achromatic, m = L for r, g, and b
+  if (hsl.s === 0) {
+    const result = Math.round(hsl.l * 255);
+    return { r: result, g: result, b: result };
+  }
+  // calculate chroma based on value of L
+  const c =
+    adjustedL < 0.5
+      ? 2 * adjustedL * adjustedS
+      : 2 * adjustedS * (1 - adjustedL);
+  // calculate m
+  const m = adjustedL - c / 2;
+  // return r, g, and b rounded to the nearest integer
+  return {
+    r: Math.round(255 * (m + hueToRgb(c, hsl.h + 120))),
+    g: Math.round(255 * (m + hueToRgb(c, hsl.h))),
+    b: Math.round(255 * (m + hueToRgb(c, hsl.h - 120))),
+  };
+};
+exports.hslToRgb = hslToRgb;
+// Step 2. Convert RGB to HEX
+const rgbToHex = (r, g, b) => {
+  return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+};
+exports.rgbToHex = rgbToHex;
