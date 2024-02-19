5928082a42674f175ba4a8bfdf5a67339147bbd0
richardhaddadau <richard@haddads.net.au>
2024-02-19 17:08:23 +1000

    remove username from me page for now

--- src/app/me/page.js
+++ src/app/me/page.js
@@ -33,10 +33,7 @@ const Me = observer(() => {
     <main className="w-full h-full rounded-lg">
       <div className="flex flex-row w-full h-full text-primary-dark dark:text-primary-light">
         <section className="p-5 md:p-6 w-full h-full rounded-sm bg-dashboard-primary-light dark:bg-dashboard-primary-dark">
-          <h2 className="mb-2 md:mb-2 pb-2 md:pb-4 text-xl w-full">
-            Welcome
-            {" " + userStore.userData.username || ""}!
-          </h2>
+          <h2 className="mb-2 md:mb-2 pb-2 md:pb-4 text-xl w-full">Welcome!</h2>
           <Stack sx={{ width: "100%", color: "grey.500" }} spacing={2}>
             <LinearProgress color="inherit" />
           </Stack>
