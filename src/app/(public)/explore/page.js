8a9cd20c36e0dc698e92308a6bc8a93cf9b879d6
richardhaddadau <richard@haddads.net.au>
2024-02-29 23:18:34 +1000

    fixed explore page. No need for layout

--- src/app/(public)/explore/page.js
+++ src/app/(public)/explore/page.js
@@ -3,6 +3,12 @@ import Footer from "@/components/Footer";
 import ExploreBlock from "@/components/ExploreBlock";
 import SearchBlock from "@/components/SearchBlock";
 
+export const metadata = {
+  title: "Explore | My Pop Off",
+  description:
+    "Browse and search through My Pop Off's vast directory of creatives and amazing influencers!",
+};
+
 export default function Explore() {
   return (
     <main className="flex flex-col items-center justify-start overflow-hidden">
