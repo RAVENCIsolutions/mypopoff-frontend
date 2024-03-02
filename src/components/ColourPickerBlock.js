af6a210039b7b15ccd2605ad2744c6dbcd30b644
richardhaddadau <richard@haddads.net.au>
2024-03-02 15:01:03 +1000

    more optimised colour picker

--- src/components/ColourPickerBlock.js
+++ src/components/ColourPickerBlock.js
@@ -1,36 +1,33 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
 import { observer } from "mobx-react";
-
-import { FaSlackHash } from "react-icons/fa";
-
+import { HexColorPicker } from "react-colorful";
+import { useEffect, useRef, useState } from "react";
+import { CircularProgress, Stack } from "@mui/material";
 import useOutsideClick from "@/utility/useOutsideClick";
-import { isValidHex, removeLeadingHash } from "@/utility/colourUtils";
+
+import {
+  addLeadingHash,
+  handleHex,
+  isValidHex,
+  removeLeadingHash,
+} from "@/utility/colourUtils";
 
 import userStore from "@/stores/UserStore";
 
+import { FaSlackHash } from "react-icons/fa";
+
 const ColourPickerBlock = observer(({ customisation }) => {
   const [showPicker, setShowPicker] = useState(false);
-  const [chosenColour, setChosenColour] = useState(null);
+  const [pickerColour, setPickerColour] = useState("");
+  const [inputColour, setInputColour] = useState("");
+
+  const [inputTimeout, setInputTimeout] = useState(null);
+  const [checkingInput, setCheckingInput] = useState(false);
 
   const ref = useRef(null);
 
   const { userData } = userStore;
-
-  const defaultColours = [
-    "#B80000",
-    "#DB3E00",
-    "#FCCB00",
-    "#008B02",
-    "#006B76",
-    "#1273DE",
-    "#004DCF",
-    "#5300EB",
-    "#FFFFFF",
-    "#333333",
-    "#000000",
-  ];
 
   const addToPalette = (key, value) => {
     const newPalette = {
@@ -41,128 +38,107 @@ const ColourPickerBlock = observer(({ customisation }) => {
     userStore.setPalette(newPalette);
   };
 
-  useOutsideClick(ref, () => {
-    if (isValidHex(chosenColour)) {
-      addToPalette(customisation, `#${removeLeadingHash(chosenColour)}`);
-    } else {
-      setChosenColour(userData.palette[customisation]);
-    }
+  const setColor = (value) => {
+    setCheckingInput(true);
 
-    setShowPicker(false);
+    const newHex = handleHex(value)
+      ? addLeadingHash(handleHex(value))
+      : addLeadingHash(value);
+
+    setTimeout(() => {
+      if (isValidHex(newHex)) {
+        setInputColour(newHex);
+        setPickerColour(newHex);
+        addToPalette(customisation, newHex);
+      } else {
+        setInputColour(userData.palette[customisation]);
+        setPickerColour(userData.palette[customisation]);
+      }
+
+      setCheckingInput(false);
+    }, 1000);
+  };
+
+  useOutsideClick(ref, () => {
+    if (showPicker) {
+      setColor(inputColour);
+      setShowPicker(false);
+    }
   });
 
   useEffect(() => {
     if (userData) {
-      setChosenColour(userData.palette[customisation]);
+      setInputColour(userData.palette[customisation]);
+      setPickerColour(userData.palette[customisation]);
     }
   }, []);
 
+  useEffect(() => {
+    return () => clearTimeout(inputTimeout);
+  }, []);
+
   return (
-    <article
-      ref={ref}
-      className="flex flex-col items-start w-fit text-dashboard-primary-dark dark:text-dashboard-primary-light/80"
-      onFocus={() => {
-        setShowPicker(true);
-      }}
-    >
-      <section
-        className="mb-2 flex items-center gap-2"
-        onClick={() => {
-          setShowPicker(!showPicker);
-        }}
-      >
+    <article className="flex flex-col items-start w-fit text-dashboard-primary-dark dark:text-dashboard-primary-light/80">
+      <section className={`mb-2 flex items-center gap-2`} ref={ref}>
         <div
           className={`block w-8 h-8 rounded-full border-2 border-dashboard-primary-dark shadow-lg shadow-primary-dark/10`}
           style={{
-            backgroundColor: showPicker
-              ? `#${removeLeadingHash(chosenColour)}`
-              : userData.palette[customisation],
+            backgroundColor: inputColour,
+          }}
+          onClick={() => {
+            setShowPicker(!showPicker);
           }}
         ></div>
-        <p className={`text-sm font-bold uppercase`}>
-          {showPicker
-            ? `#${removeLeadingHash(chosenColour)}`
-            : userStore.userData.palette[customisation]}
-        </p>
+        <div className={`relative`}>
+          <input
+            type={"text"}
+            className={`py-1 px-6 rounded-full w-[6.5rem] outline-none text-sm font-bold uppercase`}
+            value={removeLeadingHash(inputColour)}
+            maxLength={6}
+            onChange={(event) => {
+              setInputColour(event.target.value);
+
+              if (inputTimeout) clearTimeout(inputTimeout);
+
+              const newTimeout = setTimeout(() => {
+                setColor(event.target.value);
+              }, 1000);
+
+              setInputTimeout(newTimeout);
+            }}
+            onClick={() => {
+              setShowPicker(true);
+            }}
+          />
+          <FaSlackHash
+            size={12}
+            className={`absolute top-1/2 left-2 -translate-y-1/2 text-primary-dark/50 dark:text-primary-light/50`}
+          />
+          {checkingInput ? (
+            <Stack
+              className={`absolute top-1/2 right-2 -translate-y-1/2`}
+              sx={{ color: "grey.500" }}
+              spacing={2}
+            >
+              <CircularProgress color="inherit" size={12} />
+            </Stack>
+          ) : null}
+        </div>
       </section>
       {showPicker && (
         <div
           className={`p-2 flex flex-col bg-dashboard-primary-dark/20 dark:bg-dashboard-primary-light/30 rounded-lg`}
         >
-          <div
-            className={`pb-3 flex flex-wrap gap-1 border-b-[1px] border-dashboard-primary-dark/20`}
-          >
-            {defaultColours.map((colour, index) => (
-              <div
-                key={`picker-${index}`}
-                className={`block w-3 h-3 rounded-sm`}
-                style={{
-                  backgroundColor: colour,
-                }}
-                onClick={() => {
-                  addToPalette(
-                    customisation,
-                    `#${removeLeadingHash(chosenColour)}`,
-                  );
+          <HexColorPicker
+            color={pickerColour}
+            onChange={(newColour) => {
+              setPickerColour(newColour);
+              setInputColour(newColour);
 
-                  setChosenColour(colour);
-                }}
-              ></div>
-            ))}
-          </div>
-          <div className={`pt-2 flex items-center`}>
-            <div
-              className={`mr-2 block w-2 h-2 rounded-sm shadow-sm shadow-primary-dark/10`}
-              style={{
-                backgroundColor: `#${removeLeadingHash(chosenColour)}`,
-              }}
-            ></div>
-
-            <FaSlackHash size={12} />
-
-            <input
-              type="text"
-              value={removeLeadingHash(chosenColour.toUpperCase())}
-              className={`p-0.5 w-14 text-xs font-sans tracking-wider font-bold uppercase bg-dashboard-primary-light dark:bg-dashboard-primary-dark outline-none`}
-              maxLength={6}
-              onChange={(e) => {
-                const lastChar = e.target.value[e.target.value.length - 1];
-
-                if (/([a-f\d])$/i.test(lastChar)) {
-                  setChosenColour(e.target.value);
-                }
-
-                if (e.target.value.length === 6 && isValidHex(e.target.value)) {
-                  addToPalette(
-                    customisation,
-                    `#${removeLeadingHash(chosenColour)}`,
-                  );
-                }
-              }}
-              onBlur={() => {
-                if (isValidHex(chosenColour)) {
-                  addToPalette(
-                    customisation,
-                    `#${removeLeadingHash(chosenColour)}`,
-                  );
-                } else {
-                  setChosenColour(userData.palette[customisation]);
-                }
-              }}
-            />
-          </div>
+              addToPalette(customisation, newColour);
+            }}
+          />
         </div>
-        // <CompactPicker
-        //   color={userData.palette[customisation]}
-        //   onChange={(e) => {
-        //     const newPalette = {
-        //       ...userData.palette,
-        //       [customisation]: e.hex,
-        //     };
-        //
-        //     userStore.setPalette(newPalette);
-        //   }}
-        // />
       )}
     </article>
   );
