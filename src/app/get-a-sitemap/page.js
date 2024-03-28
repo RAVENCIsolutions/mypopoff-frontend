4aac50a887467519274624cdd168d1c4761a4b5c
richardhaddadau <richard@haddads.net.au>
2024-03-28 21:20:18 +1000

    no cache for netlify + sitemap

---
+++ src/app/get-a-sitemap/page.js
@@ -0,0 +1,27 @@
+﻿import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import SitemapGenerator from "@/components/SitemapGenerator";
+
+export default async function SiteMapPage() {
+  const supabase = await createServerComponentClient({ cookies });
+
+  // grab all usernames
+  const { data, error } = await supabase.from("users").select("username");
+
+  let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+
+  if (data)
+    data.forEach((user, index) => {
+      const url = `${process.env.NEXT_PUBLIC_HOME_ROUTE}${user.username}`;
+      sitemap += `
+  <url>
+      <loc>${url}</loc>
+  </url>`;
+    });
+
+  sitemap += `
+</urlset>`;
+
+  return <SitemapGenerator data={sitemap} />;
+}
---
+++ src/components/SitemapGenerator.js
@@ -0,0 +1,34 @@
+﻿"use client";
+
+import { useEffect, useState } from "react";
+
+const SitemapGenerator = ({ data }) => {
+  const [downloaded, setDownloaded] = useState(false);
+
+  useEffect(() => {
+    if (downloaded) return;
+
+    const blob = new Blob([data], { type: "application/xml" });
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", "sitemap.xml");
+
+    document.body.appendChild(link);
+    link.click();
+
+    URL.revokeObjectURL(url);
+    link.parentNode.removeChild(link);
+
+    setDownloaded(true);
+  }, []);
+
+  return (
+    <div>
+      <p>Downloading sitemap...</p>
+    </div>
+  );
+};
+
+export default SitemapGenerator;
